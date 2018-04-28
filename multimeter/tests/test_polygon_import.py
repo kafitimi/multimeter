6cda75ace7fd81eae234abd272a56d1975623bf5
cadfoot <indiegeargames@gmail.com>
2018-04-28 20:59:16 +0900

    added tests for problem statement and solution (tutorial)

--- multimeter/tests/test_polygon_import.py
+++ multimeter/tests/test_polygon_import.py
@@ -22,6 +22,12 @@ class TestPolygonImport(TestCase):
         checker_source_path = 'multimeter\\tests\\test_problem\\files\\check.cpp'
         with open(checker_source_path) as checker_source_file:
             self.checker_source = checker_source_file.read()
+        statement_source_path = 'multimeter\\tests\\test_problem\\statements\\english\\problem.tex'
+        with open(statement_source_path) as file:
+            self.statement_source = file.read()
+        tutorial_source_path = 'multimeter\\tests\\test_problem\\statements\\english\\tutorial.tex'
+        with open(tutorial_source_path) as file:
+            self.tutorial_source = file.read()
 
     def test_base_data(self):
         problem = Problem.objects.get(name='Fire stations')
@@ -34,3 +40,11 @@ class TestPolygonImport(TestCase):
     def test_checker_source(self):
         problem = Problem.objects.get(name='Fire stations')
         self.assertEqual(problem.checker, self.checker_source)
+
+    def test_statement_source(self):
+        problem = Problem.objects.get(name='Fire stations')
+        self.assertEqual(problem.conditions, self.statement_source)
+
+    def test_solution_source(self):
+        problem = Problem.objects.get(name='Fire stations')
+        self.assertEqual(problem.solutions, self.tutorial_source)
