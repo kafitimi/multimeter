71665f531fc6281a9304b981d84cb9e60a6056b7
Volodya Leveryev <volodya.leveryev@gmail.com>
2018-10-04 14:01:17 +0900

    Инклюжн-теги для бутстрап-форм

--- multimeter/templatetags/multimeter_tags.py
+++ multimeter/templatetags/multimeter_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.forms import BoundField
 
 register = template.Library()
 
@@ -13,3 +14,67 @@ def form_control(bound_field):
         classes.append('is-invalid')
     attributes['class'] = ' '.join(classes)
     return bound_field
+
+
+@register.inclusion_tag('multimeter/form_control.html')
+def form_control(form_field: BoundField, col: str = '') -> dict:
+    """
+    Делаем бутстраповский форм-контрол из джанговского поля формы
+    Фичи:
+    - если после валидации нашлись косяки, показываем их
+    - если в поле есть хелп-текст, то показываем его
+    - опционально передаем в form-group CSS-класс для выстраивания полей в грид (например col, col-3 и т.п.)
+    """
+
+    # Если в форме нет такого поля, то нужно сообщить об этом куда следует
+    try:
+        attributes = form_field.field.widget.attrs
+    except AttributeError as e:
+        print('Form does not have such field: %s' % str(form_field))
+        raise e
+
+    # Список CSS-классов оформления, который нужно навесить на тег input
+    css_classes = ['form-control']
+
+    # Подсветим красненьким косячные поля
+    if form_field.errors:
+        css_classes.append('is-invalid')
+
+    # Сохраним имеющиеся CSS-классы оформления
+    if 'class' in attributes:
+        css_classes.append(attributes['class'])
+
+    attributes['class'] = ' '.join(css_classes)
+    return {'col': col, 'form_field': form_field, 'help': str(form_field.help_text)}
+
+
+@register.inclusion_tag('multimeter/form_check.html')
+def form_check(form_field: BoundField, col: str = '') -> dict:
+    """
+    Делаем бутстраповский чекбокс из джанговского поля формы
+    Фичи:
+    - если после валидации нашлись косяки, показываем их
+    - если в поле есть хелп-текст, то показываем его
+    - опционально передаем в form-group CSS-класс для выстраивания полей в грид (например col, col-3 и т.п.)
+    """
+
+    # Если в форме нет такого поля, то нужно сообщить об этом куда следует
+    try:
+        attributes = form_field.field.widget.attrs
+    except AttributeError as e:
+        print('Form does not have such field: %s' % str(form_field))
+        raise e
+
+    # Список CSS-классов оформления, который нужно навесить на тег input
+    css_classes = ['form-check-input']
+
+    # Подсветим красненьким косячные поля
+    if form_field.errors:
+        css_classes.append('is-invalid')
+
+    # Сохраним имеющиеся CSS-классы оформления
+    if 'class' in attributes:
+        css_classes.append(attributes['class'])
+
+    attributes['class'] = ' '.join(css_classes)
+    return {'col': col, 'form_field': form_field, 'help': str(form_field.help_text)}
