c056f13e55e6ba231167c12c0375505277e1f3d9
cadfoot <indiegeargames@gmail.com>
2018-11-04 00:36:01 +0900

    auth returns user

--- multimeter/auth.py
+++ multimeter/auth.py
@@ -4,17 +4,16 @@ from multimeter.models import Account
 
 def login(request, cleaned_data):
 	user = authenticate(request, **cleaned_data)
-	if user is None:
-		return False
-	django_login(request, user)
-	return True
+	if user is not None:
+		django_login(request, user)
+	return user
 
 
 def signup(request, cleaned_data):
 	username = cleaned_data['username']
 	email = cleaned_data['email']
 	if Account.objects.filter(username=username) or Account.objects.filter(email=email):
-		return False
+		return None
 	user = Account.objects.create_user(**cleaned_data)
 	user.save()
 	return login(request, cleaned_data)
