450e44832b436e85e38acd2a698984774f070660
cadfoot <indiegeargames@gmail.com>
2019-05-26 00:29:14 +0900

    создание директории очереди проверки при запуске приложения

--- multimeter/apps.py
+++ multimeter/apps.py
@@ -1,9 +1,18 @@
 """ Настройки приложения """
 
 from django.apps import AppConfig
+from django.conf import settings
+import os
 
 
 class MultimeterConfig(AppConfig):
     """ Конфигурация приложения """
     name = 'multimeter'
     verbose_name = 'Мультиметр'
+
+    def ready(self):
+        try:
+            os.mkdir(settings.SUBMISSION_QUEUE_DIR)
+            print('Submission queue directory is missing. Creating one.')
+        except FileExistsError:
+            print('Submission queue directory already exists.')
